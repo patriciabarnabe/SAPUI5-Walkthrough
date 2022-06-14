0ecdec56a43237b566ae819ec0265c3fc047bf21
Patrícia Barnabé <patbarnabe5@gmail.com>
2022-06-14 20:59:35 -0300

    Steps 21 and 22

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -5,3 +5,10 @@ sap.ui.define([
    return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
    });
 });
+
+
+
+
+
+// https://sapui5.hana.ondemand.com/sdk/#/entity/sap.m.tutorial.walkthrough
+// https://sapui5.hana.ondemand.com/sdk/#/topic/3da5f4be63264db99f2e5b04c5e853db
